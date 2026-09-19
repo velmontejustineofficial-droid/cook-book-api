86aaf97abc19cfe1f5ba9427e8a42d29b29bf0e5
bon110603 <velmontejustineofficial@gmail.com>
2026-09-19 14:41:57 +0800

    another setup

--- core/config/env.js
+++ core/config/env.js
@@ -4,5 +4,5 @@ export const env = {
     nodeEnv: environment.NODE_ENV || "development",
     port: Number(environment.PORT) || 3000,
 
-    databaseUrl: environment.DATABASE_URL,
+    databaseUrl: environment.DATABASE_URL || "",
 };
