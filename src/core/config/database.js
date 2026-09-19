ee6b08fc191bca128ca7296cf1857b32222515fa
bon110603 <velmontejustineofficial@gmail.com>
2026-09-19 12:13:03 +0800

    core setup

--- src/core/config/database.js
+++ src/core/config/database.js
@@ -3,6 +3,11 @@ import { env } from "../config/env.js";
 
 const { Pool } = pg;
 
+console.log(
+    "DATABASE_URL loaded:",
+    Boolean(env.databaseUrl)
+);
+
 const pool = new Pool({
     connectionString: env.databaseUrl,
 
