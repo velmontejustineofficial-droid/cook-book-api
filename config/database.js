3c95dbc1aedbe19b33c5b006ae41651869c86357
bon110603 <velmontejustineofficial@gmail.com>
2026-09-19 13:16:56 +0800

    connection local test success

--- config/database.js
+++ config/database.js
@@ -3,11 +3,16 @@ import "dotenv/config";
 
 const { Pool } = pg;
 
+console.log(
+    "DATABASE_URL:",
+    process.env.DATABASE_URL ? "LOADED" : "NOT LOADED"
+);
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 export default pool;
